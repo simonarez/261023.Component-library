056c7605d733631a68aa6250b8cfac8d22fd7930
simonarez <85300793+simonarez@users.noreply.github.com>
2023-11-02 15:48:13 +0100

    Re-made javascript

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
+
 const overviewButtons = document.querySelectorAll(".overview__button");
 const components = document.querySelectorAll(".component");
 
-overviewButtons.forEach((overviewButton) => {
-  const toggleComponentClass = (event) => {
-    const button = event.currentTarget;
-    const componentShow = button.dataset.componentShow;
+overviewButtons.forEach((button) => {
+	button.addEventListener("click", displayComponent);
+  });
 
-    components.forEach((component) => {
-      component.classList.remove("component--visible");
+  
+function displayComponent (event){
 
-      const componentName = component.dataset.componentName;
+	const button = event.currentTarget;
 
-      if (componentShow === componentName) {
-        component.classList.add("component--visible");
-      } else {
-        component.classList.remove("component--visible");
-      }
-    });
+	components.forEach((component) => {
+		component.classList.remove("component--visible");
+        const componentName = component.dataset.componentName;
+		const componentToDisplay = button.dataset.componentShow;
 
-    components.forEach((button) => {
-      button.classList.remove("overview__button--active");
 
-      const componentName = button.dataset.componentName;
-      if (componentShow === componentName) {
-        button.classList.add("overview__button--active");
-      } else {
-        button.classList.remove("overview__button--active");
-      }
-    });
-  };
+		if(componentToDisplay === componentName){
+		   component.classList.add("component--visible");
+		}
 
-  overviewButton.addEventListener("click", toggleComponentClass);
-});
+	}
+)};
+
